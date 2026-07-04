76057f21daa23b4186f5c25e9fe9865ae1d626e4
Konasani Teja Maheswara Reddy <2200080216.aids@gmail.com>
2026-07-04 15:03:40 +0530

    Fix GitHub Pages asset paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
-}
-module.exports = nextConfig
+
+  basePath: isProd ? '/zero-noise-frontend' : '',
+  assetPrefix: isProd ? '/zero-noise-frontend/' : '',
+};
+
+module.exports = nextConfig;
